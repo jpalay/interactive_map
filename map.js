5a3533908f928be170fbccd0ee048c435a30d25b
Josh Palay <joshpalay@gmail.com>
2013-03-02 16:35:03 -0500

    changed some style and js

--- map.js
+++ map.js
@@ -1,8 +1,10 @@
 $(function(){
     $("#stop1").mouseup(function() {
         $("#sloths").attr("style", "display:inline");
+        $("#stage").hide();
     }); 
     $("#sloths").mouseup(function(){
     	$(this).hide();
+    	$("#stage").show();
     });
 });
